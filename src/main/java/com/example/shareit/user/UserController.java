f1485ba4ac7fe61b6081f9f2f7567a7450184bbc
Apelsin545 <dmitri_ch@list.ru>
2024-04-06 15:21:23 +0300

    feat: создать UserController

---
+++ src/main/java/com/example/shareit/user/UserController.java
@@ -0,0 +1,14 @@
+package com.example.shareit.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
