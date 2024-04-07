44bad9d8af30d51aa448d4633d607e77a0542707
Apelsin545 <dmitri_ch@list.ru>
2024-04-07 14:00:30 +0300

    feat: ItemRepository injection

--- src/main/java/com/example/shareit/item/ItemServiceImpl.java
+++ src/main/java/com/example/shareit/item/ItemServiceImpl.java
@@ -1,11 +1,19 @@
 package com.example.shareit.item;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ItemServiceImpl implements ItemService {
+    private final ItemRepository items;
+
+    @Autowired
+    public ItemServiceImpl(ItemRepository items) {
+        this.items = items;
+    }
+
     @Override
     public Item addItem(Item item) {
         return null;
