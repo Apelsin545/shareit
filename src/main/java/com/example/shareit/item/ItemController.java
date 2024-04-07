4a27b80e2cd134b5bea3e150a86d247cef2c28f9
Apelsin545 <dmitri_ch@list.ru>
2024-04-07 14:07:43 +0300

    feat: создать эндпоинт, для поиска вещи по id

--- src/main/java/com/example/shareit/item/ItemController.java
+++ src/main/java/com/example/shareit/item/ItemController.java
@@ -1,6 +1,8 @@
 package com.example.shareit.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,8 @@ public class ItemController {
         this.itemService = itemService;
     }
 
-    
+    @GetMapping("/items/{itemId}")
+    public Item getItemById(@PathVariable long itemId) {
+        return itemService.getItemById(itemId);
+    }
 }
